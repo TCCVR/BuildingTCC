1891f351ae60cf552af4c11641611978ff5a96ec
Ellen Laureto <ellen.barbosa@usp.br>
2022-07-16 18:29:26 -0300

    rotation changes

--- Assets/Scripts/BuildingManager.cs
+++ Assets/Scripts/BuildingManager.cs
@@ -9,18 +9,36 @@ public class BuildingManager : MonoBehaviour
     //[SerializeField] private Transform pfWoodHarvester;
     [SerializeField] private Transform playerTransform;
     [SerializeField] private BuildingTypeSO activeBuildingType;
+    private int counter;
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
         {
             Vector3 mouseWorldPosition =  Mouse3D.GetMouseWorldPosition();
-            Debug.Log(mouseWorldPosition);
+            //Debug.Log(mouseWorldPosition);
             float maxBuildDistance = 10f;
+
             if (Vector3.Distance(playerTransform.position, mouseWorldPosition)<maxBuildDistance) {
-                Instantiate(activeBuildingType.prefab, mouseWorldPosition, Quaternion.identity);
+                Instantiate(activeBuildingType.prefab, mouseWorldPosition, transform.rotation * Quaternion.Euler(0, 45*counter, 0));
             }
             
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            counter += 1;
+            if (counter == 8)
+            {
+                counter = 0;
+            }
+            print(counter);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            counter = 0;
+            print("Counter Reset");
         }
     }
 
@@ -33,4 +51,5 @@ public class BuildingManager : MonoBehaviour
     {
         return activeBuildingType;
     }
+
 }
